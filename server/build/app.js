3c5a065126eb69e37e3f27b14b9b39a386c9da12
Prithiviraj M <prithiviraj@colanonline.com>
2024-06-26 15:22:08 +0530

    remove build

--- server/build/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./envConfig/index.env.config");
-const server_connection_1 = require("./loaders/server/server.connection");
-const server = new server_connection_1.ServerConnection();
-server.connect();
-//# sourceMappingURL=app.js.map
--- server/build/envConfig/index.env.config.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'; 
-import path from 'path';
-
-const env:string           = process.env.NODE_ENV || 'development';
-const envFilePath:string   = path.resolve(__dirname, `../envConfig/.env.${env}`);
-
-dotenv.config({ path: envFilePath, debug: false  });
